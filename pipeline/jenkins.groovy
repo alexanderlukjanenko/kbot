fd6bc06b09df73ee0ae84c81c4eccf65ebf61b5b
alexanderlukjanenko <alexander.lukjanenko@gmail.com>
2024-04-19 16:01:15 +0000

    pick arch

--- pipeline/jenkins.groovy
+++ pipeline/jenkins.groovy
@@ -4,6 +4,8 @@ pipeline {
 
         choice(name: 'OS', choices: ['linux', 'darwin', 'windows', 'all'], description: 'Pick OS')
 
+        choice(name: 'ARCH', choices: ['amd64', 'arm64'], description: 'Pick ARCH')
+
     }
     stages {
         stage('Example') {
