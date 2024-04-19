fe5ee0b967efec7c9e520e785ecb0be7708c05bb
alexanderlukjanenko <alexander.lukjanenko@gmail.com>
2024-04-19 16:10:14 +0000

    f

--- pipeline/jenkins.groovy
+++ pipeline/jenkins.groovy
@@ -7,6 +7,10 @@ pipeline {
         choice(name: 'ARCH', choices: ['amd64', 'arm64'], description: 'Pick ARCH')
 
     }
+    environment {
+        REPO = 'https://github.com/alexanderlukjanenko/kbot'
+        BRANCH = 'main'
+    }
     stages {
         stage('Example') {
             steps {
@@ -14,6 +18,20 @@ pipeline {
 
                 echo "Build for arch: ${params.ARCH}"
 
+            }
+        }
+
+        stage("clone") {
+            steps {
+                echo 'CLONE REPO'
+                git branch: "${BRANCH}", url: "${REPO}"
+            }
+        }
+
+        stage("test") {
+            steps {
+                echo "Test"
+                sh 'make test'
             }
         }
     }
